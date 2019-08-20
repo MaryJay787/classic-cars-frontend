c86a5f0e4c0aaf59138ccce132a2d8ece36ef3cc
Aeisha Hicks <ashicks2017@outlook.com>
2019-08-20 13:40:47 -0400

    add photo

--- src/components/userprofile.js
+++ src/components/userprofile.js
@@ -6,55 +6,45 @@ import { Button, Icon } from 'semantic-ui-react'
 export default class UserProfile extends React.Component{
     render (){
         return (
-            <div>
-            <container>
-           
-            <div class="ui secondary pointing menu">
-            <a class="active item">
-              Home
-            </a>
-            <a class="item">
-              View My Car Colliections
-            </a>
-            <a class="item">
-              Add Cars 
-            </a>
-            <div class="right menu">
-              <a class="ui item">
-                Logout
-              </a>
-            </div>
-          </div>
+        <div>
+        <container>
+         <div class="ui secondary pointing menu">
+          <a class="active item">Home </a>
+          <a class="item">View My Car Colliections </a>
+          <a class="item">Add Cars </a>
+           <div class="right menu">
+            <a class="ui item">Logout</a>
+         </div>
+         </div>
+
           <h1>Flatiron Classic Cars</h1>
-          <div class="ui segment">
-            <p></p>
-            <div class="ui card">
-            <div class="image">
-              <img src="/images/avatar2/large/kristy.png"/>
-            </div>
-            <div class="content">
-              <a class="header">Kristy</a>
-              <div class="meta">
-                <span class="date">Joined in 2013</span>
-              </div>
-              <div class="description">
-                Kristy is an art director living in New York.
-              </div>
-            </div>
-            <div class="extra content">
-              <a>
-              <i class="car icon"></i>
-                Number of User Cards 
-              </a>
-            </div>
-          </div>
-           </div> 
-            </container>
-            <container>
-             <Header>Kristy Cars Collections</Header>
-             <p>Input User Cars Collection in this Container</p>
-           </container>
-          </div> //end of div
+        <div class="ui segment">
+          <p></p>
+        <div class="ui card">
+        <div class="image">
+          <img src="https://secureservercdn.net/184.168.47.225/b8a.cee.myftpupload.com/wp-content/uploads/2013/09/Screen-Shot-2013-09-19-at-4.59.44-PM.png"/>
+        </div>
+        <div class="content">
+          <a class="header">Kristy</a>
+        <div class="meta">
+          <span class="date">Joined in 2013</span>
+        </div>
+        <div class="description">Kristy is an art director living in New York. </div>
+        </div>
+        <div class="extra content">
+           <a>
+           <i class="car icon"></i>
+            Number of User Cards 
+            </a>
+        </div>
+        </div>
+        </div> 
+        </container>
+        <container>
+        <Header>Kristy Cars Collections</Header>
+        <p>Input User Cars Collection in this Container</p>
+        </container>
+        </div> //end of div
          
         
         )
