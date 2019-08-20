51ff3d0f9fd320808abd0ef497d762af1508e39f
MaryJay787 <josephmaryam.mj@gmail.com>
2019-08-20 11:28:52 -0400

    working on car collection

--- src/components/cars-collection.js
+++ src/components/cars-collection.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import CarCard from '../containers/car-card'
-import { Header, Segment, Container, Grid, Divider } from 'semantic-ui-react'
+import { Header, Segment, Container, Grid, Divider, Button } from 'semantic-ui-react'
 
 
 class CarsCollection extends React.Component {
-   
+   constructor(){
+       super()
+       this.state={
+        loggedInStatus: false,
+        currentUser: []
+       }
+   }
+
+    componentDidMount() {
+      this.checkLoginStatus();
+    }
+
+    checkLoginStatus() {
+        fetch("http://localhost:3001/login", { withCredentials: true })
+        .then(response => {
+           if (
+          this.state.loggedInStatus === false
+        ) {
+          this.setState({
+            loggedInStatus: true,
+            currentUser: response.user
+          });
+          this.props.history.push("/userprofile")
+
+        } else if (
+          (this.state.loggedInStatus === true)
+        ) {
+          this.setState({
+            loggedInStatus: false
+          });
+        }
+      })
+      .catch(error => {
+        console.log("check login error", error);
+        });
+    }
 
   render(){
       return(
           <div>
             <Segment>
             <Header as='h1' textAlign='center' >Classic Car Collection</Header>
+            <Link to="/login">{this.state.loggedInStatus ? <Button content="Login"/> : null}</Link>
             </Segment>
             <Segment>
                 <Container>
