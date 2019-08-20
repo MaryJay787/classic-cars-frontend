ff7e8d5466e14cdb25331d53bd75f7dfcdc6a96c
MaryJay787 <josephmaryam.mj@gmail.com>
2019-08-20 12:12:40 -0400

    working oncard functionality

--- src/components/cars-collection.js
+++ src/components/cars-collection.js
@@ -5,43 +5,6 @@ import { Header, Segment, Container, Grid, Divider, Button } from 'semantic-ui-r
 
 
 class CarsCollection extends React.Component {
-   constructor(){
-       super()
-       this.state={
-        loggedInStatus: false,
-        currentUser: []
-       }
-   }
-
-    componentDidMount() {
-      this.checkLoginStatus();
-    }
-
-    checkLoginStatus() {
-        fetch("http://localhost:3001/login", { withCredentials: true })
-        .then(response => {
-           if (
-          this.state.loggedInStatus === false
-        ) {
-          this.setState({
-            loggedInStatus: true,
-            currentUser: response.user
-          });
-          this.props.history.push("/userprofile")
-
-        } else if (
-          (this.state.loggedInStatus === true)
-        ) {
-          this.setState({
-            loggedInStatus: false
-          });
-        }
-      })
-      .catch(error => {
-        console.log("check login error", error);
-        });
-    }
-
   render(){
       return(
           <div>
@@ -52,7 +15,7 @@ class CarsCollection extends React.Component {
             <Segment>
                 <Container>
                     <Grid centered>
-                     {this.props.cars.map((car, idx) => <CarCard key={idx} car={car}/> )}
+                     {this.props.cars.map((car, idx) => <CarCard user={this.props.user} loginStatus={this.props.loginStatus} key={idx} car={car}/> )}
                       <Divider hidden/>
                     </Grid>
                 </Container>
