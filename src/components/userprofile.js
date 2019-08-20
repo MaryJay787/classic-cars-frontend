978ebe4490d954a6dcdb79f352e74bbf6fe8a639
Aeisha Hicks <ashicks2017@outlook.com>
2019-08-20 11:10:33 -0400

    add profile update

--- src/components/userprofile.js
+++ src/components/userprofile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Card, Image } from 'semantic-ui-react'
+import { Header, Card, Image, Container } from 'semantic-ui-react'
 import { Button, Icon } from 'semantic-ui-react'
 
 
@@ -7,39 +7,64 @@ export default class UserProfile extends React.Component{
     render (){
         return (
             <div>
-            <Header as='h2' textAlign='center'>User Profile</Header>
-            <Card centered size='large'>
-                <Image src='https://cdn.dealeraccelerate.com/rkm/1/6969/440191/790x1024/1967-pontiac-gto' wrapped ui={false} />
-                <Card.Content>
-                <Card.Header>{this.props.user.username}</Card.Header>
-                <Card.Meta>
-                    <span className='date'>Joined in 2015</span>
-                </Card.Meta>
-                <Card.Description>
-                    Matthew is a musician living in Nashville.
-                </Card.Description>
-                </Card.Content>
-               
-            </Card>
-            <Button animated >
-            <Button.Content visible>My Car Collection</Button.Content>
-            <Button.Content hidden>
-                <Icon name='arrow right' />
-            </Button.Content>
-            </Button>
-            <Button animated='vertical' floated='right'>
-            <Button.Content hidden> <Icon name='shop' /></Button.Content>
-            <Button.Content visible>
-                Flatiron Car Collection
-            </Button.Content>
-            </Button>
-            <Button animated='fade'>
-            <Button.Content visible>Edit Profile</Button.Content>
-            <Button.Content hidden> <Icon name='arrow right' /></Button.Content>
-            </Button>
+            <container>
+           
+            <div class="ui secondary pointing menu">
+            <a class="active item">
+              Home
+            </a>
+            <a class="item">
+              View My Car Colliections
+            </a>
+            <a class="item">
+              Add Cars 
+            </a>
+            <div class="right menu">
+              <a class="ui item">
+                Logout
+              </a>
             </div>
+          </div>
+          <h1>Flatiron Classic Cars</h1>
+          <div class="ui segment">
+            <p></p>
+            <div class="ui card">
+            <div class="image">
+              <img src="/images/avatar2/large/kristy.png"/>
+            </div>
+            <div class="content">
+              <a class="header">Kristy</a>
+              <div class="meta">
+                <span class="date">Joined in 2013</span>
+              </div>
+              <div class="description">
+                Kristy is an art director living in New York.
+              </div>
+            </div>
+            <div class="extra content">
+              <a>
+              <i class="car icon"></i>
+                Number of User Cards 
+              </a>
+            </div>
+          </div>
+           </div> 
+            </container>
+            <container>
+             <Header>Kristy Cars Collections</Header>
+             <p>Input User Cars Collection in this Container</p>
+           </container>
+          </div> //end of div
+         
+        
         )
     }
 
 }
+
+
+
+
+
+            
 
