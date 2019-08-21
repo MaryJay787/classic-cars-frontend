beb0458a4662fba186d7cca78afb8c7803b87132
MaryJay787 <josephmaryam.mj@gmail.com>
2019-08-21 14:47:41 -0400

    updated signup page

--- src/components/signup-page.js
+++ src/components/signup-page.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Segment, Container, Grid, Header, Divider } from 'semantic-ui-react';
 
 class SignUpPage extends React.Component{
 
     render(){
         return(
-                <Form onSubmit={this.props.handleSignSubmit}>  
-                    <Form.Field>
-                    <label>image</label>
-                    <input placeholder='Image Url' name='image' onChange={this.props.handleSignIChange} />
-                    </Form.Field>
-                    <Form.Field>
-                    <label>username</label>
-                    <input placeholder='Username' name='username' onChange={this.props.handleSignUChange} />
-                    </Form.Field>
-                    <Form.Field>
-                    <label>password</label>
-                    <input placeholder='Password' name='password' onChange={this.props.handleSignPChange} />
-                    </Form.Field>
-                    <Button type='submit' content="Submit"/>
-                </Form>
+            <Segment padded='very'>
+                <Header content=" Create Your Classic Cars Account!"  textAlign='center'/>
+                <Divider/>
+                <Container textAlign="center">
+                    <Grid centered>
+                        <Form onSubmit={this.props.handleSignSubmit} >  
+                            <Form.Field required>
+                            <label>Profile Picture</label>
+                            <input placeholder='Image Url' name='image' onChange={this.props.handleSignIChange} />
+                            </Form.Field>
+                            <Form.Field required>
+                            <label>Your UserName</label>
+                            <input placeholder='Username' name='username' onChange={this.props.handleSignUChange} />
+                            </Form.Field>
+                            <Form.Field required>
+                            <label>Your Password</label>
+                            <input placeholder='Password' name='password' onChange={this.props.handleSignPChange} />
+                            </Form.Field>
+                            <Button type='submit' content="Submit"/>
+                        </Form>
+                    </Grid>
+                </Container>
+            </Segment>
         )
     }
 }
